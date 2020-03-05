c946fb0e1c380f112c128434c59120e3649142c0
Roy <github@webroy.de>
2020-03-05 21:29:30 +0100

    change DB user

--- src/PlayWithDb.java
+++ src/PlayWithDb.java
@@ -19,7 +19,7 @@ public class PlayWithDb {
      */
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         //Class.forName("com.mysql.jdbc.Driver");  
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stadlerquartett", "root", "root");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stadlerquartett", "learning", "learning");
 
         Statement stmt = con.createStatement();
         
